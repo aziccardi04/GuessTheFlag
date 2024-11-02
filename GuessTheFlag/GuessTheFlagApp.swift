25f91350db956dd4bbc9b15f5135519c7998c97b
Alfie Ziccardi <aziccardi04@outlook.com>
2024-11-02 14:13:49 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alfie Ziccardi on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
